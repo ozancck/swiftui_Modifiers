b5340b7349c729ffec0919ffb183c863bbfe6a44
Ozan Çiçek <132375017+infoisteportal@users.noreply.github.com>
2024-07-23 14:43:45 +0300

    Initial Commit

---
+++ Modifiers/ModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModifiersApp.swift
+//  Modifiers
+//
+//  Created by Ozan Çiçek on 23.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
